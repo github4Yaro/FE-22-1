b4bbddaccc577bf7f834838827de69c62e24e358
zloyzluka <zloyzluka@gmail.com>
2022-11-03 20:43:22 +0200

    Add my IP info

---
+++ myIP.js
@@ -0,0 +1,17 @@
+const IPlink = 'http://ip-api.com/json/';
+const GAPI = 'http://www.7timer.info/bin/astro.php?ac=0&lang=en&unit=metric&output=internal&tzshift=0';
+
+	fetch(IPlink).then(r => r.json()).then( data => {
+		const IPaddr = document.getElementById('ipAddr');
+		const country = document.getElementById('country');
+		const region = document.getElementById('region');
+		const city = document.getElementById('city');
+		const GeoImg = document.getElementById('GeoImg');
+
+		IPaddr.innerText = data.query;
+		country.innerText = data.country;
+		region.innerText = data.regionName;
+		city.innerText = data.city;
+		link = `${GAPI}&lon=${data.lon}&lat=${data.lan}`;
+		GeoImg.src = link;
+	});
