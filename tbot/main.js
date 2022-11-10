dc252064a9002cccce8aeacdff5500c56caa7a7d
Yaro <zloyzluka@gmail.com>
2022-11-10 14:54:08 +0200

    debug

--- tbot/main.js
+++ tbot/main.js
@@ -6,7 +6,7 @@
     const initDataUnsafe = Telegram.WebApp.initDataUnsafe || {};
 
     const userInfoLabel = document.getElementById('user_info');
-    userInfoLabel.innerText = `${Telegram.WebAppUser}`
+    userInfoLabel.innerText = `${JSON.stringify(initData.user)}`
 
 	Telegram.WebApp.MainButton
         .setText('Закрыть окно')
