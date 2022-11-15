1634e933b57018a37a2fcb59f48be257d42812d1
Yaro <zloyzluka@gmail.com>
2022-11-15 11:12:28 +0200

    update

--- tbot/components/calendar/calendar_model.js
+++ tbot/components/calendar/calendar_model.js
@@ -13,7 +13,7 @@ export default class calendarModel {
 
 	constructor(controllerHandleUpdate){
         this.controllerHandleUpdate = controllerHandleUpdate;
-        this.url = '//ec2-18-233-147-122.compute-1.amazonaws.com/getData'; 
+        this.url = '//yarotbot.tk/getData'; 
     }
 	
 	getMonthRecords = (month, year, userID) => {
